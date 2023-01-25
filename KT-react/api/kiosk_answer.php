42419d38ed52962028210d3021d43d00b84fe4c9
minho2618 <minho2618@naver.com>
2023-01-25 14:37:54 +0900

    백엔드 코드 수정

--- KT-react/api/kiosk_answer.php
+++ KT-react/api/kiosk_answer.php
@@ -11,7 +11,43 @@ $conn = $objDb->connect();
 $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'POST':
-        // 별도의 POST 요청은 받지 않음
+        $input = json_decode(file_get_contents('php://input'));
+
+        $sql = "SELECT * FROM kiosk_answer WHERE store = :store AND level = :level";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(':store', $input->store);
+        $stmt->bindParam(':level', $input->level);
+
+        if ($stmt->execute()) {
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            echo "stmt execute failed";
+            return;
+        }
+        $json_answer = json_encode($input->basket);
+        $answer = json_decode($json_answer, true);
+
+        // print_r($answer);
+        // print_r($result);
+        $result_basket = json_decode($result["basket"], true);
+
+        // print_r($result_basket);
+
+        ksort($result_basket);
+        ksort($answer);
+
+
+        $return1 = array_diff_assoc_recursive($result_basket, $answer);
+        $return2 = array_diff_assoc_recursive($answer, $result_basket);
+
+        $return = array(
+            'in' => $return1,
+            'not_in' => $return2
+        );
+
+        echo json_encode($return);
+
         break;
 
     case 'GET':
@@ -28,4 +64,28 @@ switch ($method) {
         echo json_encode($result);
 
         break;
+}
+
+
+function array_diff_assoc_recursive($array1, $array2)
+{
+    $difference=array();
+    foreach ($array1 as $key => $value) {
+        if ($key === "name") {
+            $difference[$key] = $value;
+        }
+        if (is_array($value)) {
+            if (!isset($array2[$key]) || !is_array($array2[$key])) {
+                $difference[$key] = $value;
+            } else {
+                $new_diff = array_diff_assoc_recursive($value, $array2[$key]);
+                if (!empty($new_diff)) {
+                    $difference[$key] = $new_diff;
+                }
+            }
+        } elseif (!array_key_exists($key, $array2) || $array2[$key] !== $value) {
+            $difference[$key] = $value;
+        }
+    }
+    return $difference;
 }
